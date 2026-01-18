b6d0e5883e7dd2dea3b429a0f88b30596e8de2e0
ribhavsharma <ribhavsharma2003@gmail.com>
2026-01-18 15:44:49 -0800

    feat: store user edits in db

--- src/capstone_project_team_5/services/__init__.py
+++ src/capstone_project_team_5/services/__init__.py
@@ -1,6 +1,17 @@
 """Services"""
 
+from capstone_project_team_5.services.portfolio_persistence import (
+    create_portfolio_item,
+    get_latest_portfolio_item_for_project,
+    update_portfolio_item,
+)
 from capstone_project_team_5.services.ranking import update_project_ranks
 from capstone_project_team_5.services.upload import upload_zip
 
-__all__ = ["update_project_ranks", "upload_zip"]
+__all__ = [
+    "update_project_ranks",
+    "upload_zip",
+    "create_portfolio_item",
+    "get_latest_portfolio_item_for_project",
+    "update_portfolio_item",
+]
