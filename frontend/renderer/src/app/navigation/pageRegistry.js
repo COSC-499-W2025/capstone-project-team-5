ce522a33da9c0f29fca3c6c29a39f6f93e5753e0
Ojus Sharma <ojus@student.ubc.ca>
2026-03-11 21:40:53 -0700

    add ProfilePage to navigation and page registry

--- frontend/renderer/src/app/navigation/pageRegistry.js
+++ frontend/renderer/src/app/navigation/pageRegistry.js
@@ -2,6 +2,7 @@ import DashboardPage from '../../pages/dashboard/DashboardPage'
 import ProjectsPage from '../../pages/projects/ProjectsPage'
 import ExperiencePage from '../../pages/experience/ExperiencePage'
 import EducationPage from '../../pages/education/EducationPage'
+import ProfilePage from '../../pages/profile/ProfilePage'
 import PortfolioPage from '../../pages/portfolio/PortfolioPage'
 import ComingSoonPage from '../../pages/shared/ComingSoonPage'
 
@@ -10,6 +11,7 @@ const PAGE_COMPONENTS = {
   projects: ProjectsPage,
   experience: ExperiencePage,
   education: EducationPage,
+  profile: ProfilePage,
   portfolio: PortfolioPage,
 }
 
