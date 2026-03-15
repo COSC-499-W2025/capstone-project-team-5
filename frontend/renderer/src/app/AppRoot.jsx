e40730639594d7dcf8fe7f41a7f07c43e2aa78fc
Ethan Methorst <ethan.methorst1@gmail.com>
2026-03-14 19:36:11 -0700

    Added cache.

--- frontend/renderer/src/app/AppRoot.jsx
+++ frontend/renderer/src/app/AppRoot.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { AppContext } from './context/AppContext'
 import { useAppBootstrap } from './hooks/useAppBootstrap'
 import ConsentSetup from '../pages/consents/ConsentSetup'
@@ -13,6 +13,9 @@ const INITIAL_UPLOAD_HIGHLIGHTS = {
 export default function AppRoot() {
   const [page, setPage] = useState('dashboard')
   const [uploadHighlights, setUploadHighlights] = useState(INITIAL_UPLOAD_HIGHLIGHTS)
+
+  const analysisCache = useRef({})
+
   const {
     consentReady,
     setConsentReady,
@@ -47,6 +50,7 @@ export default function AppRoot() {
         setPage,
         uploadHighlights,
         setUploadHighlights,
+        analysisCache,
       }}
     >
       <AppShell page={page} setPage={setPage} apiOk={apiOk} user={user} />
